c4ff2d6389f55dcddcf9467618d2464e9072d88b
Vito Mantese <vmantese@gmail.com>
2019-03-07 13:34:36 -0600

    added scan logic

--- cmd/ddb-copy/main.go
+++ cmd/ddb-copy/main.go
@@ -10,5 +10,44 @@ import (
 )
 
 func main() {
+	//flags from command line
+	fromTable := flag.String("src","","from which table")
+	toTable   := flag.String("dest","","to which table")
+	awsRegion := flag.String("aws-region","","which AWS Region")
+	flag.Parse()
 
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: awsRegion,
+		},
+	)
+	if err != nill{
+		fmt.Println("Cannot connect to AWS, error was:")
+		panic(err)
+	}
+
+	db := dynamodb.New(sess)
+	err = db.ScanPages(&dynamodb.ScanInput{
+		TableName:	fromTable,
+		ConsistentRead: aws.Bool(true),
+	}, func(scanOutput * dynamodb.ScanOutput, lastPage bool) bool {
+		for _, item := range scanOutput.Items {
+			input := &dynamodb.PutItemInput{
+				Item:	item,
+				TableName: toTable,
+			}
+
+			_, err = db.PutItem(input)
+
+			if err != nill {
+				fmt.Println("Error with inserting item:")
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+		}
+		return !lastPage
+	})
+	if err != nill {
+		fmt.Println(err)
+	}
 }
