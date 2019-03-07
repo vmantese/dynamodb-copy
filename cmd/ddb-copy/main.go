9224e774b6274c2c2aae9d17bade80cf30be8e1e
Vito Mantese <vmantese@gmail.com>
2019-03-07 13:17:39 -0600

    first commit w/ imports

---
+++ cmd/ddb-copy/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/session"
+	"github.com/aws/aws-sdk-go/dynamodb"
+	"os"
+)
+
+func main() {
+
+}
